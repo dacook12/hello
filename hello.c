0f2cb21f389a0d0f85f40eeeb9e3ed75172f3003
David Cook <dcook12@huskers.unl.edu>
2020-08-24 13:43:27 +0000

    initial commit

--- hello.c
+++ hello.c
@@ -1,15 +1,17 @@
 /**
  * Author: David Cook
- * Date: 2020/8/18
- * 
+ * Date: 2020/8/24
+ *
  * My first hello world program
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    
+
     printf("David Cook!\n");
-    printf("Computer Science")
-    return 0;
+    printf("Computer Science");
+
+    return 0\n;
+
 }
